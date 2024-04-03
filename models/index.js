80c761c3f6b05229bf3a740823b39908791c1154
The Other Promise <150410378+noe-bee@users.noreply.github.com>
2024-04-03 12:17:58 -0700

    initial commit

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const User = require('./User');
+const Project = require('./Project');
+const Comment = require('./Comment')
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.hasMany(Comment, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Project, Comment };
